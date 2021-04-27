136a53707def28ae106fea43808201e8ecb066e3
atomic-ozzy <74189145+atomic-ozzy@users.noreply.github.com>
2021-04-27 08:55:48 +0100

    Create thing.java

---
+++ thing.java
@@ -0,0 +1,10 @@
+public class scriptjj {
+
+    public static void main(String[] args) {
+
+        int shit = 69;
+        char shit2 = 'a';
+        long shit3 = 6969;
+        double shit4 = 6.9
+    }
+}
