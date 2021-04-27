23c901e625b902cebd6f059295aba9da06993a2e
atomic-ozzy <74189145+atomic-ozzy@users.noreply.github.com>
2021-04-27 08:53:43 +0100

    Create thing.cs

---
+++ thing.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int shit = 69;
+            Console.WriteLine(shit);
+            
+            string name = Console.ReadLine();
+            
+            int age = Console.ReadLine();
+
+        }
+    }
+}
